99d569a25e50cc3fc5f64d69fca8dc1dff873ae4
Aly <me@s5.pm>
2022-10-19 04:43:31 -0700

    Add comments

--- main.s
+++ main.s
@@ -1,4 +1,10 @@
+; libSystem.write
+;     (fd : int/i32 [x0], buf : *u8 [x1], count : size_t/u64 [x2])
+;     -> (written : ssize_t/i64)
 .extern _write
+; libSystem.exit
+;     (status : int/i32)
+;     -> nothing
 .extern _exit
 
 .global _main
@@ -6,15 +12,20 @@
 
 .text
 _main:
+    ; fd = stdout
     mov x0, #1
+    ; buf = &hello_world_str
     adrp x1, hello_world_str@PAGE
     add x1, x1, hello_world_str@PAGEOFF
+    ; count = 14
     mov x2, #14
     bl _write
 
+    ; status = 0
     mov x0, #0
     bl _exit
 
 .data
+; [14]u8 hello_world_str
 hello_world_str:
     .ascii "Hello, world!\n"
